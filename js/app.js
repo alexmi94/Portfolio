c65090804d629ca6e0e4df8567e8b2e624fddad6
alexmi94 <alexandrecance94@gmail.com>
2022-02-14 21:00:08 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Load");
+
+    let footer_p = document.querySelector("footer p");
+    footer_p.innerHTML += "&copy; " + new Date().getFullYear() + " -  Alexandre Cance "
+
+    let img = document.querySelector("ul li img");
+
+    let button = document.querySelector("ul li img");
+    let html = document.querySelector("html");
+
+    img.addEventListener("click",() => {
+        console.log("click");
+        html.classList.toggle("color_access");
+        button.classList.toggle("color_access_title");
+    });
+
+});
